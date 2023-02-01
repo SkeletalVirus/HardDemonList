ad5d04ec53d2f67d8ae045687e32a2fd7e51594e
RoboBear04 <116104045+RoboBear04@users.noreply.github.com>
2023-02-01 11:09:55 -0700

    Update score.js

--- js/score.js
+++ js/score.js
@@ -1,6 +1,7 @@
 /**
  * Numbers of decimal digits to round to
  -0.909091(rank)+100.909091
+ 100 / Math.sqrt((rank - 1) / 50 + 0.444444) - 50
  */
 const scale = 3;
 
@@ -16,7 +17,7 @@ export function score(rank, percent, minPercent) {
         return 0;
     }
 
-    let score = (100 / Math.sqrt((rank - 1) / 50 + 0.444444) - 50) *
+    let score = (-0.909091(rank)+100.909091) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
