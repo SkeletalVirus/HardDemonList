c6d7fc421e53668252722acc3fd64a7900b02b92
RoboBear04 <116104045+RoboBear04@users.noreply.github.com>
2023-04-10 12:00:49 -0600

    Update Roulette.js

--- js/pages/Roulette.js
+++ js/pages/Roulette.js
@@ -186,9 +186,9 @@ export default {
                 video: lvl.verification,
             }));
             const list = [];
-            if (this.useMainList) list.push(...fullListMapped.slice(0, 50));
+            if (this.useMainList) list.push(...fullListMapped.slice(0, 75));
             if (this.useExtendedList) {
-                list.push(...fullListMapped.slice(50, 100));
+                list.push(...fullListMapped.slice(75, 150));
             }
 
             // random 100 levels
